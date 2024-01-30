07849928ccd70063bdaf34c9ad89eccab0eba3bc
attila20030 <152779773+attila20030@users.noreply.github.com>
2024-01-30 11:21:57 +0100

    Add files via upload
    kasztolás feladat

---
+++ kasztolas.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class kasztolas{ 
+  public static void main(String[] args) { 
+    int myInt = 8; 
+    double myDouble = myInt;
+
+    System.out.println(myInt); 
+    System.out.println(myDouble);
+
+    byte myByte = 100; 
+    int myNum = (int)myInt;
+    
+    System.out.println(myByte); 
+    System.out.println(myNum);
+
+    char myGrade = 'B';
+    char myVar1 = 65;
+
+    System.out.println(myGrade); 
+    System.out.println(myVar1);
+    
+    boolean isJavaFun = true; 
+    boolean isFishTasty = false;
+
+    System.out.println(isJavaFun); 
+    System.out.println(isFishTasty);
+
+    String greeting = "2024.01.30 orai munka";
+    System.out.println(greeting);
+
+  }
+}
