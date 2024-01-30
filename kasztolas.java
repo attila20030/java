f1638c0b6fc44c257560129ee8a5337c875b3367
attila20030 <152779773+attila20030@users.noreply.github.com>
2024-01-30 20:59:43 +0100

    kasztolás feladat

--- kasztolas.java
+++ kasztolas.java
@@ -1,33 +1,39 @@
-import java.util.Scanner;
 
-public class kasztolas{ 
-  public static void main(String[] args) { 
-    int myInt = 8; 
-    double myDouble = myInt;
+public class kasztolas {
+    public static void main(String[] args)
+    {
+        double d = 99.1;
+        long l = (long)d;
+        int i = (int)l;
+        byte b = 11;
+        char c = 'a';
+        short s = 2024;
+        float f = 7.87f;
 
-    System.out.println(myInt); 
-    System.out.println(myDouble);
+        System.out.println("Double value " + d);
+        System.out.println("Long value " + l);
+        System.out.println("Int value " + i);
+        System.out.println("Byte value " + b);
+        System.out.println("Char value " + c);
+        System.out.println("Short value " + s);
+        System.out.println("Float value " + f);
+        System.out.println("String value " + "Hello Java");
 
-    byte myByte = 100; 
-    int myNum = (int)myInt;
-    
-    System.out.println(myByte); 
-    System.out.println(myNum);
+        double doubleValue = 2024.2023;
+        
+        float floatValue = (float) doubleValue;
+        long longValue = (long) floatValue;
+        int intValue = (int) longValue;
+        char charValue = (char) intValue;
+        short shortValue = (short) charValue;
 
-    char myGrade = 'B';
-    char myVar1 = 65;
+        byte byteValue = (byte) shortValue;
+        System.out.println("Double-ből Floatba: " + floatValue);
+        System.out.println("Float-ból Longba: " + longValue);
+        System.out.println("Longból-Intbe: " + intValue);
+        System.out.println("Intből-Charba: " + charValue);
+        System.out.println("Char-ból Shortba: " + shortValue);
+        System.out.println("Short-ból Byteba: " + byteValue);
 
-    System.out.println(myGrade); 
-    System.out.println(myVar1);
-    
-    boolean isJavaFun = true; 
-    boolean isFishTasty = false;
-
-    System.out.println(isJavaFun); 
-    System.out.println(isFishTasty);
-
-    String greeting = "2024.01.30 orai munka";
-    System.out.println(greeting);
-
-  }
+    }
 }
