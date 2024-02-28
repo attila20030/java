238a5119b2d1c269bcb84777217549fed43d7fba
attila20030 <152779773+attila20030@users.noreply.github.com>
2024-02-28 08:42:34 +0100

    Create Dolgozat4.java

---
+++ Dolgozat  /Dolgozat4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Dolgozat4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Kérem adjon meg egy számot(1-10 között): ");
+        int szam1 = sc.nextInt();
+        
+        
+        sc.close();
+    }
+}
