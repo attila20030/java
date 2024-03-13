2f796043d725d5897faa15bf059e50ebb2072388
attila20030 <152779773+attila20030@users.noreply.github.com>
2024-03-13 08:28:50 +0100

    Szamol2.java

---
+++ continue,vreak/Szamol2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Szamol2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Kérem adjon meg egy számot: ");
+        int szam1 = sc.nextInt();
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % szam1 == 0) {
+                System.out.println(i);
+            }
+            
+
+            if (i == szam1 * 10) {
+                System.out.println(i);
+                break;
+            }
+        }
+        sc.close();
+    }
+}
