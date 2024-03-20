c7662ff8ada234f93cacc7f7470fe2fd7ece4ab5
attila20030 <152779773+attila20030@users.noreply.github.com>
2024-03-20 08:25:32 +0100

    ArraysMerging.java

---
+++ Array/ArraysMerging.java
@@ -0,0 +1,11 @@
+public class ArraysMerging {
+    public static void main(String[] args) {
+
+    int[] arr = { 12, 45, 67, 89, 100, 23, 3456, 897, 452, 444, 899, 700 };
+    int[] arr1 = { 10, 324, 45, 90, 9808 };
+
+    int[] osszes = new int[arr.length + arr1.length];
+    System.out.println(osszes);
+
+    }
+}
